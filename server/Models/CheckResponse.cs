576e96a10b15a97f770d71d61471e13b46b4046d
toastermagic <toastermagic@gmail.com>
2016-05-10 00:04:05 +0100

    removed .net server

--- server/Models/CheckResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace dayMonthSecond
-{
-    public class CheckResponse
-    {
-        public string ExternalAuthId { get; set; }
-        public string DmsUserId { get; set; }
-    }
-}
--- server/Properties/Auth0Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace dayMonthSecond.Properties
-{
-    public class Auth0Settings
-    {
-        public string Domain { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-    }
-}
