b83203afdd6c04892f6f25454cd4a8bf0ab98961
toastermagic <toastermagic@gmail.com>
2016-05-12 16:41:55 +0100

    further simplify express server

--- server/app.js
+++ server/app.js
@@ -4,31 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var dotenv = require('dotenv');
-var jwt = require('express-jwt');
-var cors = require('cors');
 var http = require('http');
 var compression = require('compression');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 var router = express.Router();
 
-// dotenv.load();
-
-var authenticate = jwt({
-  secret: new Buffer('XAOtQLyyK5Rf4wrE0T60BsxzYFz4g5hrYpcY4GLh7dugGXiuNJEy4bjBciMEWGrX', 'base64'),
-  audience: '0DkTCPKzFbJPEow18W1eT2yzT3VtJJTw'
-});
-
-app.use(cors());
 // view engine setup
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -40,21 +24,12 @@ app.use(compression());
 
 app.use(express.static(__dirname + '/../wwwroot'));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/secured', authenticate);
-app.use('/vendor', express.static(__dirname + '/../node_modules'));
-
-app.get('/ping', function(req, res) {
-  res.send("All good. You don't need to be authenticated to call this");
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.sendfile('index.html', {root: __dirname + '/../wwwroot'});
 });
 
-
-app.get('/secured/ping', function(req, res) {
-  res.status(200).send("All good. You only get this message if you're authenticated");
-});
-
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 5000;
 
 http.createServer(app).listen(port, function (err) {
   console.log('listening in http://localhost:' + port);
