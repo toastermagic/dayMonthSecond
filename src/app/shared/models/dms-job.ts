d613c71a35defc4f420daf6ab3c1fada3c3f3573
toastermagic <toastermagic@gmail.com>
2016-05-15 16:15:54 +0100

    update DmsJob model

--- src/app/shared/models/dms-job.ts
+++ src/app/shared/models/dms-job.ts
@@ -1,9 +1,10 @@
 export class DmsJob {
-    $key: string;
-    id: string;
-    name: string;
-    created: number;
-    lastUpdated: number;
-    nextPollDue: number;
-    intervalMinutes: number;
+    $key: string;           // firebase generated pushUrl
+    name: string;           // display name for job
+    created: number;        // date job created, number because firebase don't do dates
+    lastPoll: number;       // time of last ping, number because firebase don't do dates
+    nextPollDue: number;    // deadline for next ping, number because firebase don't do dates
+    notify: string;         // address to notify in case of overdue notification
+    overdue: number;        // number of minutes after deadline to notify overdue
+    interval: number;       // number of minutes between expected invocations of job
 }
